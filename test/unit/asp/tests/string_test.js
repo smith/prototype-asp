faf1ff90d9da5ac21e7cc7fbeb27005f96eb68f2
Nathan L Smith <nlloyds@gmail.com>
2009-02-22 14:03:50 -0600

    String test updates

--- test/unit/asp/tests/string_test.js
+++ test/unit/asp/tests/string_test.js
@@ -494,7 +494,8 @@ var testResults = new Test.Unit.Runner({
     var dangerous = '{});attackTarget = "attack succeeded!";({}';
 
     // use smaller huge string size for KHTML
-    var size = navigator.userAgent.include('KHTML') ? 20 : 100;
+    //var size = navigator.userAgent.include('KHTML') ? 20 : 100;
+    var size = 100;
     var longString = '"' + '123456789\\"'.times(size * 10) + '"';
     var object = '{' + longString + ': ' + longString + '},';
     var huge = '[' + object.times(size) + '{"test": 123}]';
